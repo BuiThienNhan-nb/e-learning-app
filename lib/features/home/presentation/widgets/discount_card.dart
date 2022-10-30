d0ea94cc22afb30f21189d969e500a21cabc811d
Thien Nhan <buithiennhan2016@gmail.com>
2022-10-30 13:45:37 +0700

    [CHORE] update discount_card

--- lib/features/home/presentation/widgets/discount_card.dart
+++ lib/features/home/presentation/widgets/discount_card.dart
@@ -1,9 +1,10 @@
+import 'package:coupon_uikit/coupon_uikit.dart';
+import 'package:e_learning_app/configs/colors.dart';
+import 'package:e_learning_app/configs/dimens.dart';
+import 'package:e_learning_app/configs/styles.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../../configs/colors.dart';
-import '../../../../configs/dimens.dart';
-import '../../../../configs/styles.dart';
+import '../../../../configs/formats.dart';
 
 class DiscountCard extends StatelessWidget {
   const DiscountCard({super.key});
@@ -11,73 +12,95 @@ class DiscountCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(AppDimens.largeHeightDimens),
       decoration: BoxDecoration(
-        color: AppColors.secondaryColor,
-        borderRadius: BorderRadius.circular(AppDimens.extraItemRadius),
-        // gradient: const LinearGradient(
-        //   colors: [
-        //     AppColors.cornflowerBlueColor,
-        //     AppColors.primaryColor,
-        //   ],
-        //   stops: [
-        //     0,
-        //     0.8,
-        //   ],
-        //   begin: Alignment.topLeft,
-        // ),
-      ),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          SizedBox(
-            width: AppDimens.appDesignSize.width -
-                (AppDimens.extraLargeWidthDimens * 2),
-            child: RichText(
-              maxLines: 5,
-              overflow: TextOverflow.ellipsis,
-              text: TextSpan(
-                style: AppStyles.headline6TextStyle.copyWith(
-                  color: AppColors.whiteColor,
-                  fontWeight: FontWeight.bold,
-                ),
-                children: [
-                  TextSpan(
-                    text: "20% OFF\n",
-                    style: AppStyles.headline6TextStyle.copyWith(
-                      color: AppColors.whiteColor,
-                      fontWeight: FontWeight.normal,
-                      fontSize: 16.sp,
-                    ),
-                  ),
-                  const TextSpan(
-                    text: "TODAY SPECIAL\n\n",
-                  ),
-                  const TextSpan(
-                    text:
-                        "Get a discount for every course order! Only valid today!a a a a a aaaaaaaaaaaaaaaaaaaaaa",
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            right: 0,
-            top: -AppDimens.mediumHeightDimens,
-            child: Text(
-              "40%",
-              style: AppStyles.headline3TextStyle.copyWith(
-                color: AppColors.whiteColor,
-                shadows: [
-                  Shadow(
-                    color: AppColors.neutral.shade200.withOpacity(0.8),
-                    blurRadius: 20,
-                  ),
-                ],
-              ),
-            ),
+        borderRadius: BorderRadius.circular(AppDimens.itemRadius),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.neutral.shade400,
+            blurRadius: AppDimens.mediumHeightDimens,
+            // offset: const Offset(1, 1),
+            blurStyle: BlurStyle.normal,
+            spreadRadius: 0.2,
           ),
         ],
+      ),
+      child: CouponCard(
+        curveAxis: Axis.vertical,
+        borderRadius: AppDimens.itemRadius,
+        backgroundColor: AppColors.whiteColor,
+        curvePosition: AppDimens.itemRadius * 6,
+        firstChild: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: Image.asset(
+                "assets/icons/discount_rabbit_icon.png",
+                fit: BoxFit.scaleDown,
+              ),
+            ),
+            SizedBox(
+              width: AppDimens.smallWidthDimens / 2,
+              child: Column(
+                children: List.generate(
+                  16,
+                  (index) => Expanded(
+                    child: Container(
+                      width: AppDimens.smallWidthDimens / 2,
+                      color: index % 2 == 0
+                          ? AppColors.neutral.shade500
+                          : Colors.transparent,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        secondChild: Padding(
+          padding: EdgeInsets.only(
+            left: AppDimens.mediumWidthDimens,
+            right: AppDimens.mediumHeightDimens,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: AppDimens.mediumHeightDimens),
+              Text(
+                "TODAY SPECIAL - 35% OFF",
+                style: AppStyles.headline6TextStyle.copyWith(
+                  fontWeight: FontWeight.w800,
+                  color: AppColors.primaryColor,
+                ),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              SizedBox(height: AppDimens.smallHeightDimens),
+              Text(
+                "Expiry date: ${AppFormats.instance.formatDay.format(DateTime.now().add(const Duration(days: 1)))}",
+                style: AppStyles.subtitle2TextStyle.copyWith(
+                  color: AppColors.neutral.shade600,
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Text(
+                      "USE NOW",
+                      style: AppStyles.subtitle1TextStyle.copyWith(
+                        color: AppColors.primaryColor,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

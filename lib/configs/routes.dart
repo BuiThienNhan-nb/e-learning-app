c87faa0ca53811d824876d0d873edaa4aec22785
Thien Nhan <buithiennhan2016@gmail.com>
2022-11-13 17:58:36 +0700

    [CHORE] fix go_router bottom_bar_navigator

--- lib/configs/routes.dart
+++ lib/configs/routes.dart
@@ -1,7 +1,3 @@
-import 'package:e_learning_app/features/auth/forgot_password/presentation/pages/forgot_password_page.dart';
-import 'package:e_learning_app/features/auth/forgot_password/presentation/state/providers/forgot_password_provider.dart';
-import 'package:e_learning_app/features/home/presentation/pages/home_page.dart';
-import 'package:e_learning_app/utils/nav_bar/tab_bar_shell_route.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
@@ -9,11 +5,16 @@ import 'package:injectable/injectable.dart';
 import 'package:provider/provider.dart';
 
 import '../bases/presentation/atoms/bottom_nav_bar.dart';
+import '../features/auth/forgot_password/presentation/pages/forgot_password_page.dart';
+import '../features/auth/forgot_password/presentation/state/providers/forgot_password_provider.dart';
 import '../features/auth/sign_in/presentation/pages/sign_in_page.dart';
 import '../features/auth/sign_in/presentation/state/mobx/sign_in_store.dart';
 import '../features/auth/sign_in/presentation/state/provider/auth_page_provider.dart';
 import '../features/auth/sign_up/presentation/pages/sign_up_page.dart';
 import '../features/auth/sign_up/presentation/state/mobx/sign_up_store.dart';
+import '../features/home/presentation/pages/home_page.dart';
+import '../features/teacher_detail/presentation/pages/teacher_detail_page.dart';
+import '../utils/nav_bar/tab_bar_shell_route.dart';
 
 @lazySingleton
 class AppRoutes {
@@ -25,9 +26,11 @@ class AppRoutes {
   final String signIn = '/sign-in';
   final String signUp = '/sign-up';
   final String forgotPassword = '/forgot-password';
-  final String teacherDetail = "/teacher-detail";
-  String get initial => signIn;
-  // String get initial => _bottomBarLocator.mainPage.first;
+  // final String teacherDetail = "/teacher-detail";
+  late final String teacherDetail =
+      "${_bottomBarLocator.mainPage.first}/teacher";
+  // String get initial => signIn;
+  String get initial => _bottomBarLocator.mainPage.first;
 
   late final GoRouter router = GoRouter(
     navigatorKey: _rootNavigatorKey,
@@ -74,12 +77,35 @@ class AppRoutes {
           child: const SignUpPage(),
         ),
       ),
+      GoRoute(
+        parentNavigatorKey: _rootNavigatorKey,
+        name: "teacher",
+        path: "${_bottomBarLocator.mainPage[0]}/teacher/:teacherId",
+        builder: (context, state) {
+          // final teacher = state.extra as TeacherModel;
+          return TeacherDetailPage(
+            teacherId: state.params["teacherId"] ?? "N/A",
+          );
+        },
+      ),
       BottomTabBarShellRoute(
         tabs: _bottomBarLocator.tabs,
         routes: [
           GoRoute(
             path: _bottomBarLocator.mainPage[0],
             builder: (context, state) => const HomePage(),
+            // routes: [
+            //   GoRoute(
+            //     name: "teacher",
+            //     path: "teacher/:teacherId",
+            //     builder: (context, state) {
+            //       // final teacher = state.extra as TeacherModel;
+            //       return TeacherDetailPage(
+            //         teacherId: state.params["teacherId"] ?? "N/A",
+            //       );
+            //     },
+            //   ),
+            // ],
           ),
           GoRoute(
             path: _bottomBarLocator.mainPage[1],
@@ -94,7 +120,14 @@ class AppRoutes {
             builder: (context, state) => const Logout(),
           ),
         ],
-      )
+      ),
+      // GoRoute(
+      //   path: teacherDetail,
+      //   builder: (context, state) {
+      //     final teacher = state.extra as TeacherModel;
+      //     return TeacherDetailPage(teacher: teacher);
+      //   },
+      // ),
     ],
   );
 }
